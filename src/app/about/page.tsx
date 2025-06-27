fadd42429cd6ad86c0b5ea125d301888645b5e6d
Agbo120 <139587176+Agbo120@users.noreply.github.com>
2025-06-27 11:46:36 +0000

    Added About page and fixed structure

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <main className="flex h-screen items-center justify-center bg-green-500 text-white">
+      <h1 className="text-4xl font-bold">This is the About Page 🌍</h1>
+    </main>
+  );
+}
